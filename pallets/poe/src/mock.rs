48478728a32d850f929997d2cca124cfd9789530
greatmartial <fananbang94@gmail.com>
2021-08-20 18:11:59 +0800

    add poe pallet

---
+++ pallets/poe/src/mock.rs
@@ -0,0 +1,68 @@
+use crate as pallet_poe;
+use sp_core::H256;
+use frame_support::parameter_types;
+use sp_runtime::{
+	traits::{BlakeTwo256, IdentityLookup}, testing::Header,
+};
+use frame_system as system;
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+type Block = frame_system::mocking::MockBlock<Test>;
+
+// Configure a mock runtime to test the pallet.
+frame_support::construct_runtime!(
+	pub enum Test where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+		PoeModule: pallet_poe::{Pallet, Call, Storage, Event<T>},
+	}
+);
+
+parameter_types! {
+	pub const BlockHashCount: u64 = 250;
+	pub const SS58Prefix: u8 = 42;
+
+	// 设置proof hash key的限制
+	pub const ProofHashKeyLimit: u64 = 1000;
+}
+
+impl system::Config for Test {
+	type BaseCallFilter = ();
+	type BlockWeights = ();
+	type BlockLength = ();
+	type DbWeight = ();
+	type Origin = Origin;
+	type Call = Call;
+	type Index = u64;
+	type BlockNumber = u64;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = u64;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type Event = Event;
+	type BlockHashCount = BlockHashCount;
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = ();
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = SS58Prefix;
+	type OnSetCode = ();
+}
+
+impl pallet_poe::Config for Test {
+	type Event = Event;
+
+	// 定义proof 的hash key限制的关联类型
+	type ProofHashKeyLimit = ProofHashKeyLimit;
+}
+
+// Build genesis storage according to the mock runtime.
+pub fn new_test_ext() -> sp_io::TestExternalities {
+	system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
+}
